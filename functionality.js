0a653baac16a22f7c2386fdb2e5f1c1c4a2b7a78
Marlie Martinez <marlieahue@gmail.com>
2024-06-19 17:12:19 -0400

    working on functionality and deleted extraStuff file

--- functionality.js
+++ functionality.js
@@ -1,22 +1,45 @@
 import { fetchData } from "./poke-api.js";
+import { pokeProcess } from "./poke-img.js";
 
+const pokeData = await fetchData();
+const pokeProcessing = await pokeProcess();
+const pokeTypArr = ["grass", "fire", "water", "psychic"];
+const pokeList = document.querySelector(".pokeList");
+// console.log(pokeList)
 
-const pokeData = await fetchData()
-const pokeTypArr = ["grass", "fire", "water", "psychic"]
-
-// function showAll () {
-//     pokeList.innerHTML = '';
-//     pokeTypArr.forEach((poke) => {
-//         let pokes = document.getElementsByClassName(...poke)
-//         let newPokeList = document.createElement("div")
-//         newPokeList.innerHTML = `<b>${pokes}</b>`;
-//         pokeList.append(newPokeList)
-//     })
-// }
+export async function showAll() {
+  const btn = document.querySelector(".showAllBtn");
+  btn.addEventListener("click", (event) => {
+    console.log(event);
+    pokeList.innerHTML = "";
+    console.log(pokeList);
+    // pokeTypArr.forEach((poke) => {
+    //   let pokes = document.getElementsByClassName(...poke);
+    //   console.log(pokes);
+    //   let newPokeList = document.createElement("div");
+    //   newPokeList.innerHTML = `<b>${pokes}</b>`;
+    //   pokeList.append(newPokeList);
+    // });
+  });
+}
 // showAll()
 
-export function categorizePoke(){
-    const btn = document.querySelectorAll(".btn");
-    console.log(btn)
-    btn.addEventListener(("click"), (e) => console.log(e))
+export async function categorizePoke() {
+  pokeTypArr.forEach((el) => {
+    const btn = document.querySelector(`.${el}`);
+    console.log(btn);
+    btn.addEventListener("click", (e) => {
+      console.log(e);
+      let str = e.target.className;
+      let newArr = str.split(" ");
+      console.log(newArr)
+      pokeTypArr.forEach((i) => {
+        //     if(newArr[1] === pokeTypArr[i]){
+        // }
+      });
+    });    
+  });
+
+
+  return "im done";
 }
--- main.js
+++ main.js
@@ -1,50 +1,12 @@
 import { fetchData } from "./poke-api.js";
-import { pokeProcess } from "./poke-img.js";
-import { addingAudio } from "./extraStuff.js";
 import { categorizePoke } from "./functionality.js"
+
+
 const fetchingData = fetchData();
-const pokeProcessing = pokeProcess();
-const audioAdded = addingAudio();
-
-
+const data = await fetchData()
 const pokeTypArr = ["grass", "fire", "water", "psychic"];
 const pokeList = document.querySelector(".pokeList");
-const categoriesOfPoke = categorizePoke()
+const categoriesOfPoke = await categorizePoke()
+console.log(categoriesOfPoke)
 
 
-
-const showBtn = document.querySelector(".showAllBtn");
-showBtn.addEventListener("click", (e) => {
-    // console.log(e)
-    pokeList.innerHTML = '';
-    pokeTypArr.forEach((pokeType) => {
-        let pokes = document.getElementsByClassName(...pokeType)
-        let newPokeList = document.createElement("div")
-        newPokeList.innerHTML = `<b>${pokes}</b>`;
-        console.log(newPokeList)
-        pokeList.append(newPokeList)
-    })
-});
-
-
-// const categoriesOfPoke = document.querySelector(".btn")
-// categoriesOfPoke.addEventListener("click", (event) => {
-//     event.preventDefault()
-//     console.log(event)
-// })
-
-
-// function catergorizePoke(pokeType) {
-//   pokeList.innerHTML = "";
-//   let filtered = pokeTypArr.filter((poke) => {
-//     poke === pokeType;
-//   });
-//   filtered.forEach((poke) => {
-//     let newPokeList = document.createElement("div");
-//     let pokes = document.getElementsByClassName(poke);
-//     newPokeList.innerHTML = `<b>${pokes}</b>`;
-//     pokeList.append(newPokeList);
-//   });
-// }
-
-// catergorizePoke();
